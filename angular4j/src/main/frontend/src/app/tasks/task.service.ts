432eac2ea2b22ed394a98e32ee768e378b9f7992
Eder Anillo <anloder4@gmail.com>
2019-06-21 17:06:03 -0500

    <Angular4j> Servicio para actualizar el estado de complatado de las tareas

--- angular4j/src/main/frontend/src/app/tasks/task.service.ts
+++ angular4j/src/main/frontend/src/app/tasks/task.service.ts
@@ -1,6 +1,6 @@
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpParams} from '@angular/common/http'
 import {Injectable} from '@angular/core'
-import { map } from 'rxjs/operators';
+import { Task } from './task.model';
 
 @Injectable()
 export class TaskService{
@@ -11,5 +11,16 @@ export class TaskService{
 	getsTasks(){
 		return this.http.request("GET","/api/tasks",{responseType:"json"});
 		//this.http.get('/api/tasks').map(reponse => response.json);
+	}
+
+	saveTask(task: Task,checked: boolean){
+		task.completed = checked;
+	
+		
+		//const params = new HttpParams({fromObject:});
+		return this.http.request("POST","/api/tasks/save",{responseType:"json",body:JSON.parse(JSON.stringify(task))});
+		//var response_ = this.http.post('/api/tasks/save',task);
+		
+		//return ;
 	}
 }
